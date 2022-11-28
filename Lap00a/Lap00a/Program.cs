a9256c0df72193e89afe3ae1a28db72924bfcf6f
suthada sopab <96216472+suthada@users.noreply.github.com>
2022-11-28 16:08:22 +0700

    A good programmer must add description - professor

---
+++ Lap00a/Lap00a/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Nantawat Phaekhwdee");
+Console.WriteLine("6534502916");
+Console.WriteLine("nantawat.ph@gmail.com");
+//change
